06ef1a3d341412fc6e2f16d1bd2516af7c0b6c80
sistracia <156994197+sistracia@users.noreply.github.com>
2024-02-12 22:31:14 +0700

    feat: add `RssController`

---
+++ src/Server/Controllers/RssController.fs
@@ -0,0 +1,36 @@
+﻿namespace Server.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open System.Xml
+open System.ServiceModel.Syndication
+
+
+type RSS =
+    struct
+        val Title: string
+
+        val Link: string
+
+        new(title: string, link: string) = { Title = title; Link = link }
+    end
+
+[<ApiController>]
+[<Route("[controller]")>]
+type RssController(logger: ILogger<RssController>) =
+    inherit ControllerBase()
+
+    let parseRSSdotnet () =
+        using (XmlReader.Create("https://about.gitlab.com/atom.xml")) (fun reader ->
+            SyndicationFeed.Load(reader).Items
+            |> Seq.map (fun item ->
+                new RSS(
+                    item.Title.Text,
+                    match item.Links |> Seq.tryHead with
+                    | Some first -> first.Uri.AbsoluteUri
+                    | None -> "-"
+                )))
+
+
+    [<HttpGet>]
+    member _.Get() = async { return parseRSSdotnet () }
