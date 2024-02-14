3374cead5891c644474fbed72b9dce9ca0e39e1f
sistracia <156994197+sistracia@users.noreply.github.com>
2024-02-14 20:52:03 +0700

    Use url(s) from request for get RSS

--- web/src/Server/Server.fs
+++ web/src/Server/Server.fs
@@ -8,26 +8,43 @@ type RSS =
     struct
         val Title: string
 
+        val LastUpdatedTime: DateTime
+
         val Link: string
 
-        new(title: string, link: string) = { Title = title; Link = link }
+        new(title: string, lastUpdatedTime: DateTime, link: string) =
+            { Title = title
+              LastUpdatedTime = lastUpdatedTime
+              Link = link }
     end
 
 
-let parseRSS =
-    use reader = (XmlReader.Create("https://about.gitlab.com/atom.xml"))
+let parseRSS (url: string) =
+    async {
+        use reader = XmlReader.Create url
 
-    SyndicationFeed.Load(reader).Items
-    |> Seq.map (fun item ->
-        new RSS(
-            item.Title.Text,
-            match item.Links |> Seq.tryHead with
-            | Some first -> first.Uri.AbsoluteUri
-            | None -> "-"
-        ))
+        return
+            (SyndicationFeed.Load reader).Items
+            |> Seq.map (fun item ->
+                new RSS(
+                    item.Title.Text,
+                    item.LastUpdatedTime.DateTime,
+                    match item.Links |> Seq.tryHead with
+                    | Some first -> first.Uri.AbsoluteUri
+                    | None -> "-"
+                ))
+    }
 
-let rssIndexAction (ctx: HttpContext) = parseRSS |> Controller.json ctx
+let rssIndexAction (ctx: HttpContext) =
+    task {
+        let! rssList = ctx.Request.Query.Item("url").ToArray() |> Seq.map parseRSS |> Async.Parallel
 
+        return!
+            rssList
+            |> Array.reduce (fun acc elem -> Seq.concat [ acc; elem ])
+            |> Seq.sortByDescending (fun rss -> rss.LastUpdatedTime)
+            |> Controller.json ctx
+    }
 
 let rssController = controller { index rssIndexAction }
 
