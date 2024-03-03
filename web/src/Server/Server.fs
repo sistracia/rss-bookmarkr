a567a1f75b32c312f6a9b0a5dac9f902ec70d100
sistracia <156994197+sistracia@users.noreply.github.com>
2024-03-03 22:21:39 +0700

    Grouping Server code per module

--- web/src/Server/Server.fs
+++ web/src/Server/Server.fs
@@ -36,159 +36,168 @@ let parseRSS (url: string) =
                     | None -> "-" })
     }
 
-let getUser (loginForm: LoginForm) =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "SELECT id, username, password FROM users WHERE username = @username"
-    |> Sql.parameters [ "@username", Sql.string loginForm.Username ]
-    |> Sql.execute (fun read ->
-        { Id = read.string "id"
-          Username = read.text "username"
-          Password = read.text "password" })
-    |> List.tryHead
+module DataAccess =
 
-let insertUser (loginForm: LoginForm) =
-    let newUid = Guid.NewGuid().ToString()
+    let getUser (connectionString: string) (loginForm: LoginForm) =
+        connectionString
+        |> Sql.connect
+        |> Sql.query "SELECT id, username, password FROM users WHERE username = @username"
+        |> Sql.parameters [ "@username", Sql.string loginForm.Username ]
+        |> Sql.execute (fun read ->
+            { Id = read.string "id"
+              Username = read.text "username"
+              Password = read.text "password" })
+        |> List.tryHead
 
-    connectionString
-    |> Sql.connect
-    |> Sql.query "INSERT INTO users (id, username, password) VALUES (@id, @username, @password)"
-    |> Sql.parameters
-        [ "@id", Sql.text newUid
-          "@username", Sql.text loginForm.Username
-          "@password", Sql.text loginForm.Password ]
-    |> Sql.executeNonQuery
-    |> ignore
+    let insertUser (connectionString: string) (loginForm: LoginForm) =
+        let newUid = Guid.NewGuid().ToString()
 
-    newUid
+        connectionString
+        |> Sql.connect
+        |> Sql.query "INSERT INTO users (id, username, password) VALUES (@id, @username, @password)"
+        |> Sql.parameters
+            [ "@id", Sql.text newUid
+              "@username", Sql.text loginForm.Username
+              "@password", Sql.text loginForm.Password ]
+        |> Sql.executeNonQuery
+        |> ignore
 
-let getRSSUrls (userId: string) =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "SELECT url FROM rss_urls WHERE user_id = @user_id"
-    |> Sql.parameters [ "@user_id", Sql.string userId ]
-    |> Sql.execute (fun read -> read.text "url")
+        newUid
 
-let insertUrls (userId: string) (urls: string array) =
-    connectionString
-    |> Sql.connect
-    |> Sql.executeTransaction
-        [ "INSERT INTO rss_urls (id, url, user_id) VALUES (@id, @url, @user_id)",
-          [ yield!
-                urls
-                |> Array.map (fun url ->
-                    [ "@id", Sql.text (Guid.NewGuid().ToString())
-                      "@url", Sql.text url
-                      "@user_id", Sql.text userId ]) ] ]
-    |> ignore
+    let getRSSUrls (connectionString: string) (userId: string) =
+        connectionString
+        |> Sql.connect
+        |> Sql.query "SELECT url FROM rss_urls WHERE user_id = @user_id"
+        |> Sql.parameters [ "@user_id", Sql.string userId ]
+        |> Sql.execute (fun read -> read.text "url")
 
-let insertSession (userId: string) (sessionId: string) =
-    connectionString
-    |> Sql.connect
-    |> Sql.query "INSERT INTO sessions (id, user_id) VALUES (@id, @user_id)"
-    |> Sql.parameters [ "@id", Sql.text sessionId; "@user_id", Sql.text userId ]
-    |> Sql.executeNonQuery
-    |> ignore
+    let insertUrls (connectionString: string) (userId: string) (urls: string array) =
+        connectionString
+        |> Sql.connect
+        |> Sql.executeTransaction
+            [ "INSERT INTO rss_urls (id, url, user_id) VALUES (@id, @url, @user_id)",
+              [ yield!
+                    urls
+                    |> Array.map (fun url ->
+                        [ "@id", Sql.text (Guid.NewGuid().ToString())
+                          "@url", Sql.text url
+                          "@user_id", Sql.text userId ]) ] ]
+        |> ignore
 
-let getUserSession (sessionId) =
-    connectionString
-    |> Sql.connect
-    |> Sql.query
-        "SELECT u.id AS user_id, u.username AS user_username, u.password AS user_password FROM users u LEFT JOIN sessions s ON s.user_id = u.id WHERE s.id = @session_id"
-    |> Sql.parameters [ "@session_id", Sql.string sessionId ]
-    |> Sql.execute (fun read ->
-        { Id = read.string "user_id"
-          Username = read.text "user_username"
-          Password = read.text "user_password" })
-    |> List.tryHead
+    let insertSession (connectionString: string) (userId: string) (sessionId: string) =
+        connectionString
+        |> Sql.connect
+        |> Sql.query "INSERT INTO sessions (id, user_id) VALUES (@id, @user_id)"
+        |> Sql.parameters [ "@id", Sql.text sessionId; "@user_id", Sql.text userId ]
+        |> Sql.executeNonQuery
+        |> ignore
 
-let getRSSList (urls: string array) =
-    async {
-        let! rssList = urls |> Seq.map (fun url -> parseRSS url |> Async.Catch) |> Async.Parallel
+    let getUserSession (connectionString: string) (sessionId) =
+        connectionString
+        |> Sql.connect
+        |> Sql.query
+            "SELECT u.id AS user_id, u.username AS user_username, u.password AS user_password FROM users u LEFT JOIN sessions s ON s.user_id = u.id WHERE s.id = @session_id"
+        |> Sql.parameters [ "@session_id", Sql.string sessionId ]
+        |> Sql.execute (fun read ->
+            { Id = read.string "user_id"
+              Username = read.text "user_username"
+              Password = read.text "user_password" })
+        |> List.tryHead
 
-        return
-            rssList
-            |> Seq.map (function
-                | Choice1Of2 rss -> rss
-                | Choice2Of2 _ -> Seq.empty)
-            |> Seq.fold (fun acc elem -> Seq.concat [ acc; elem ]) []
-            |> Seq.sortByDescending (fun rss -> rss.LastUpdatedTime)
-    }
+module Handler =
+    let getRSSList (urls: string array) =
+        async {
+            let! rssList = urls |> Seq.map (fun url -> parseRSS url |> Async.Catch) |> Async.Parallel
 
-let register (sessionId: string) (loginForm: LoginForm) : LoginResponse =
-    let userId = (insertUser loginForm)
+            return
+                rssList
+                |> Seq.map (function
+                    | Choice1Of2 rss -> rss
+                    | Choice2Of2 _ -> Seq.empty)
+                |> Seq.fold (fun acc elem -> Seq.concat [ acc; elem ]) []
+                |> Seq.sortByDescending (fun rss -> rss.LastUpdatedTime)
+        }
 
-    let loginResult =
-        { LoginResult.UserId = userId
-          LoginResult.RssUrls = Array.empty
-          LoginResult.SessionId = sessionId }
+    let register (connectionString: string) (sessionId: string) (loginForm: LoginForm) : LoginResponse =
+        let userId = (DataAccess.insertUser connectionString loginForm)
 
-    Success loginResult
-
-let login (sessionId: string) (loginForm: LoginForm) (user: User) : LoginResponse =
-    if user.Password = loginForm.Password then
         let loginResult =
-            { LoginResult.UserId = user.Id
-              LoginResult.RssUrls = (getRSSUrls user.Id) |> List.toArray
+            { LoginResult.UserId = userId
+              LoginResult.RssUrls = Array.empty
               LoginResult.SessionId = sessionId }
 
         Success loginResult
-    else
-        let loginError = { LoginError.Message = "Password not match." }
-        Failed loginError
 
-let loginOrRegister (loginForm: LoginForm) : LoginResponse Async =
-    async {
-        let sessionId = Guid.NewGuid().ToString()
+    let login (connectionString: string) (sessionId: string) (loginForm: LoginForm) (user: User) : LoginResponse =
+        if user.Password = loginForm.Password then
+            let loginResult =
+                { LoginResult.UserId = user.Id
+                  LoginResult.RssUrls = (DataAccess.getRSSUrls connectionString user.Id) |> List.toArray
+                  LoginResult.SessionId = sessionId }
 
-        let loginResponse =
-            match getUser loginForm with
-            | None -> register sessionId loginForm
-            | Some user -> login sessionId loginForm user
+            Success loginResult
+        else
+            let loginError = { LoginError.Message = "Password not match." }
+            Failed loginError
 
-        match loginResponse with
-        | Success(user: LoginResult) -> insertSession user.UserId sessionId
-        | _ -> ()
+    let loginOrRegister (connectionString: string) (loginForm: LoginForm) : LoginResponse Async =
+        async {
+            let sessionId = Guid.NewGuid().ToString()
 
-        return loginResponse
-    }
+            let loginResponse =
+                match DataAccess.getUser connectionString loginForm with
+                | None -> register connectionString sessionId loginForm
+                | Some user -> login connectionString sessionId loginForm user
 
-let saveRSSUrls (userId: string, urls: string array) =
-    async {
-        let existingUrls = (getRSSUrls userId) |> List.toArray
+            match loginResponse with
+            | Success(user: LoginResult) -> DataAccess.insertSession connectionString user.UserId sessionId
+            | _ -> ()
 
-        let newUrls =
-            urls |> Array.filter (fun url -> not <| Array.contains url existingUrls)
+            return loginResponse
+        }
 
-        if newUrls.Length = 0 then
-            ()
+    let saveRSSUrls (connectionString: string) (userId: string, urls: string array) =
+        async {
+            let existingUrls = (DataAccess.getRSSUrls connectionString userId) |> List.toArray
 
-        insertUrls userId newUrls
-    }
+            let newUrls =
+                urls |> Array.filter (fun url -> not <| Array.contains url existingUrls)
 
-let initLogin (sessionId: string) : LoginResponse Async =
-    async {
-        return
-            sessionId
-            |> getUserSession
-            |> (function
-            | None ->
-                let loginError = { LoginError.Message = "Session invalid." }
-                Failed loginError
-            | Some(user: User) ->
-                let loginResult =
-                    { LoginResult.UserId = user.Id
-                      LoginResult.RssUrls = (getRSSUrls user.Id) |> List.toArray
-                      LoginResult.SessionId = sessionId }
+            if newUrls.Length = 0 then
+                ()
 
-                Success loginResult)
-    }
+            DataAccess.insertUrls connectionString userId newUrls
+        }
+
+    let initLogin (connectionString: string) (sessionId: string) : LoginResponse Async =
+        async {
+            return
+                sessionId
+                |> DataAccess.getUserSession connectionString
+                |> (function
+                | None ->
+                    let loginError = { LoginError.Message = "Session invalid." }
+                    Failed loginError
+                | Some(user: User) ->
+                    let loginResult =
+                        { LoginResult.UserId = user.Id
+                          LoginResult.RssUrls = (DataAccess.getRSSUrls connectionString user.Id) |> List.toArray
+                          LoginResult.SessionId = sessionId }
+
+                    Success loginResult)
+        }
+
+    let rssIndexAction (ctx: HttpContext) =
+        task {
+            let! rssList = ctx.Request.Query.Item("url").ToArray() |> getRSSList
+            return! rssList |> Controller.json ctx
+        }
 
 let rpcStore =
-    { getRSSList = getRSSList
-      loginOrRegister = loginOrRegister
-      saveRSSUrls = saveRSSUrls
-      initLogin = initLogin }
+    { getRSSList = Handler.getRSSList
+      loginOrRegister = (Handler.loginOrRegister connectionString)
+      saveRSSUrls = (Handler.saveRSSUrls connectionString)
+      initLogin = (Handler.initLogin connectionString) }
 
 let webApp =
     Remoting.createApi ()
@@ -196,26 +205,21 @@ let webApp =
     |> Remoting.fromValue rpcStore
     |> Remoting.buildHttpHandler
 
-let rssIndexAction (ctx: HttpContext) =
-    task {
-        let! rssList = ctx.Request.Query.Item("url").ToArray() |> getRSSList
-        return! rssList |> Controller.json ctx
-    }
+module Router =
+    let rssController = controller { index Handler.rssIndexAction }
 
-let rssController = controller { index rssIndexAction }
+    let apiRouter = router { forward "/rss" rssController }
 
-let apiRouter = router { forward "/rss" rssController }
-
-let defaultView =
-    router {
-        forward "" webApp
-        forward "/api" apiRouter
-    }
+    let defaultView =
+        router {
+            forward "" webApp
+            forward "/api" apiRouter
+        }
 
 let app =
     application {
         use_static "wwwroot"
-        use_router defaultView
+        use_router Router.defaultView
         memory_cache
         use_gzip
     }
