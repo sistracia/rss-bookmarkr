57ac75e6c5ad087615637e7ef7652672e39e37e4
sistracia <156994197+sistracia@users.noreply.github.com>
2024-04-09 17:43:22 +0700

    Use POM for e2e

---
+++ web/tests/rssbokmarkr-page.ts
@@ -0,0 +1,235 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class RSSBokmarkrPage {
+  readonly pageURL: string;
+  readonly page: Page;
+  readonly urlInput: Locator;
+  readonly addURLButton: Locator;
+  readonly deleteURLButton: Locator;
+  readonly loginFormTitle: Locator;
+  readonly loginUsernameField: Locator;
+  readonly loginPasswordField: Locator;
+  readonly saveURLsButton: Locator;
+  readonly logoutButton: Locator;
+  readonly subscribeModalButton: Locator;
+  readonly subscribeFormButton: Locator;
+  readonly subscribeEmailField: Locator;
+  readonly unsubscribeButton: Locator;
+  readonly loginModalButton: Locator;
+  readonly loginFormButton: Locator;
+  readonly skeletonLoading: Locator;
+
+  private loginResonse?: any = undefined;
+
+  readonly sessionKey = "session_id";
+
+  constructor(pageURL: string, page: Page) {
+    this.pageURL = pageURL;
+    this.page = page;
+    this.urlInput = page.getByPlaceholder("https://overreacted.io/rss.xml");
+    this.addURLButton = page.getByRole("button", { name: "Add" });
+    this.deleteURLButton = page.getByRole("button", { name: "X" });
+    this.loginFormTitle = page.getByRole("heading", { name: "Log In Form" });
+    this.loginUsernameField = page.getByPlaceholder("Username");
+    this.loginPasswordField = page.getByPlaceholder("******");
+    this.saveURLsButton = page.getByRole("button", { name: "Save Urls" });
+    this.logoutButton = page.getByText("Log Out");
+    this.subscribeModalButton = page.getByText("Subscribe").first();
+    this.subscribeFormButton = page.getByText("Subscribe").nth(2);
+    this.subscribeEmailField = page.getByPlaceholder("email@domain.com");
+    this.unsubscribeButton = page.getByRole("button", { name: "Unsubscribe" });
+    this.loginModalButton = page
+      .locator("div")
+      .filter({ hasText: /^Log In$/ })
+      .locator("label");
+    this.loginFormButton = page
+      .locator("form")
+      .getByText("Log In", { exact: true });
+    this.skeletonLoading = page.locator(".skeleton");
+  }
+
+  async goto(urls?: string[]): Promise<void> {
+    if (urls !== undefined) {
+      await this.assertRSSList({
+        expectedPostData: [urls],
+        trigger: async () => {
+          await this.page.goto(`${this.pageURL}/?url=${urls.join(",")}`);
+        },
+      });
+      return;
+    }
+
+    await this.page.goto(this.pageURL);
+  }
+
+  async expectHaveURLParam(urls: string[]) {
+    await expect(this.page).toHaveURL(`${this.pageURL}/?url=${urls.join(",")}`);
+  }
+
+  async expectNotHaveURLParam() {
+    await expect(this.page).toHaveURL(this.pageURL);
+  }
+
+  async waitLoadingPage() {
+    const firstSkeletonLoading = this.skeletonLoading.first();
+    await firstSkeletonLoading.waitFor({ state: "visible" });
+    await firstSkeletonLoading.waitFor({ state: "detached" });
+  }
+
+  async addURL(url: string) {
+    await this.urlInput.fill(url);
+
+    await this.assertRSSList({
+      expectedPostData: [[url]],
+      trigger: async () => {
+        await this.addURLButton.click();
+      },
+    });
+
+    await expect(this.urlInput).toHaveValue("");
+  }
+
+  async deleteURL(nth?: number) {
+    if (nth !== undefined) {
+      await this.deleteURLButton.nth(nth - 1).click();
+      return;
+    }
+    await this.deleteURLButton.first().click();
+  }
+
+  async saveURLs(expectedURLs: string[]) {
+    await this.assertSaveRSSUrls({
+      expectedPostData: [[this.loginResonse?.["UserId"], expectedURLs]],
+      trigger: async () => {
+        await this.saveURLsButton.click();
+      },
+    });
+  }
+
+  async subscribe(email: string) {
+    await this.subscribeModalButton.click();
+    await this.subscribeEmailField.fill(email);
+
+    await this.assertSubscribe({
+      expectedPostData: [[this.loginResonse?.["UserId"], email]],
+      trigger: async () => {
+        await this.subscribeFormButton.click();
+      },
+    });
+  }
+
+  async unsubscribe(email: string) {
+    await this.assertUnsubscribe({
+      expectedPostData: [email],
+      trigger: async () => {
+        await this.unsubscribeButton.click();
+      },
+    });
+  }
+
+  async login(username: string, password: string) {
+    await this.loginModalButton.click();
+    await expect(this.loginFormTitle).toBeVisible();
+
+    await this.loginUsernameField.fill(username);
+    await this.loginPasswordField.fill(password);
+
+    const loginResponse = await this.assertLoginOrRegister({
+      expectedPostData: [{ Username: username, Password: password }],
+      trigger: async () => {
+        await this.loginFormButton.click();
+      },
+    });
+    await this.assertLocalStorageExist(this.sessionKey);
+
+    this.loginResonse = loginResponse["Success"];
+  }
+
+  async logout() {
+    await this.logoutButton.click();
+    await this.assertLocalStorageNotExist(this.sessionKey);
+
+    this.loginResonse = undefined;
+  }
+
+  async assertRSSList(option: {
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    return await this.assertAPICall({
+      ...option,
+      url: "**/rpc/IRPCStore/getRSSList",
+    });
+  }
+
+  async assertSaveRSSUrls(option: {
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    return await this.assertAPICall({
+      ...option,
+      url: "**/rpc/IRPCStore/saveRSSUrls",
+    });
+  }
+
+  async assertSubscribe(option: {
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    return await this.assertAPICall({
+      ...option,
+      url: "**/rpc/IRPCStore/subscribe",
+    });
+  }
+
+  async assertUnsubscribe(option: {
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    return await this.assertAPICall({
+      ...option,
+      url: "**/rpc/IRPCStore/unsubscribe",
+    });
+  }
+
+  async assertLoginOrRegister(option: {
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    return await this.assertAPICall({
+      ...option,
+      url: "**/rpc/IRPCStore/loginOrRegister",
+    });
+  }
+
+  async assertAPICall(option: {
+    url: string;
+
+    trigger: () => Promise<void>;
+    expectedPostData: unknown;
+  }) {
+    const { url, trigger, expectedPostData } = option;
+
+    const responsePromise = this.page.waitForResponse(url);
+
+    await trigger();
+
+    const response = await responsePromise;
+    expect(response.ok()).toStrictEqual(true);
+    expect(response.request().postDataJSON()).toStrictEqual(expectedPostData);
+
+    return response.json();
+  }
+
+  async assertLocalStorageExist(key: string) {
+    await this.page.waitForFunction((key) => {
+      return localStorage[key] !== undefined;
+    }, key);
+  }
+
+  async assertLocalStorageNotExist(key: string) {
+    await this.page.waitForFunction((key) => {
+      return localStorage[key] === undefined;
+    }, key);
+  }
+}
--- web/tests/rssbokmarkr.spec.ts
+++ web/tests/rssbokmarkr.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect, type Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import { RSSBokmarkrPage } from "./rssbokmarkr-page";
 
 const pageURL = process.env.PAGE_URL || "http://localhost:8080";
 
 const username = "testingusername";
 const password = "testingpassword";
-const sessionKey = "session_id";
 const subscribeEmail = "test@test.com";
 
 const rssURLS = [
@@ -14,344 +14,110 @@ const rssURLS = [
 ];
 const [overreactedURL, infoqURL, stackoverflowURL] = rssURLS;
 
-async function assertRSSList(option: {
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  return await assertAPICallUnsubscribe({
-    ...option,
-    url: "**/rpc/IRPCStore/getRSSList",
-  });
-}
-
-async function assertSaveRSSUrls(option: {
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  return await assertAPICallUnsubscribe({
-    ...option,
-    url: "**/rpc/IRPCStore/saveRSSUrls",
-  });
-}
-
-async function assertSubscribe(option: {
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  return await assertAPICallUnsubscribe({
-    ...option,
-    url: "**/rpc/IRPCStore/subscribe",
-  });
-}
-
-async function assertUnsubscribe(option: {
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  return await assertAPICallUnsubscribe({
-    ...option,
-    url: "**/rpc/IRPCStore/unsubscribe",
-  });
-}
-
-async function assertLoginOrRegister(option: {
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  return await assertAPICallUnsubscribe({
-    ...option,
-    url: "**/rpc/IRPCStore/loginOrRegister",
-  });
-}
-
-async function assertAPICallUnsubscribe(option: {
-  url: string;
-  page: Page;
-  trigger: () => Promise<void>;
-  expectedPostData: unknown;
-}) {
-  const { url, page, trigger, expectedPostData } = option;
-
-  const responsePromise = page.waitForResponse(url);
-
-  await trigger();
-
-  const response = await responsePromise;
-  expect(response.ok()).toStrictEqual(true);
-  expect(response.request().postDataJSON()).toStrictEqual(expectedPostData);
-
-  return response.json();
-}
-
-function getURLInput(page: Page) {
-  return page.getByPlaceholder("https://overreacted.io/rss.xml");
-}
-
-function getAddURLButton(page: Page) {
-  return page.getByRole("button", { name: "Add" });
-}
-
-function getDeleteURLButton(page: Page) {
-  return page.getByRole("button", { name: "X" });
-}
-
-function getLoginFormTitle(page: Page) {
-  return page.getByRole("heading", { name: "Log In Form" });
-}
-
-function getLoginUsernameField(page: Page) {
-  return page.getByPlaceholder("Username");
-}
-
-function getLoginPasswordField(page: Page) {
-  return page.getByPlaceholder("******");
-}
-
-function getSaveURLsButton(page: Page) {
-  return page.getByRole("button", { name: "Save Urls" });
-}
-
-function getLogoutButton(page: Page) {
-  return page.getByText("Log Out");
-}
-
-function getSubscribeModalButton(page: Page) {
-  return page.getByText("Subscribe").first();
-}
-
-function getSubscribeFormButton(page: Page) {
-  return page.getByText("Subscribe").nth(2);
-}
-
-function getSubscribeEmailField(page: Page) {
-  return page.getByPlaceholder("email@domain.com");
-}
-
-function getUnsubscribeButton(page: Page) {
-  return page.getByRole("button", { name: "Unsubscribe" });
-}
-
-function getLoginModalButton(page: Page) {
-  return page
-    .locator("div")
-    .filter({ hasText: /^Log In$/ })
-    .locator("label");
-}
-
-function getLoginFormButton(page: Page) {
-  return page.locator("form").getByText("Log In", { exact: true });
-}
-
-async function waitLoadingPage(page: Page) {
-  await page.locator(".skeleton").first().waitFor({ state: "visible" });
-  await page.locator(".skeleton").first().waitFor({ state: "detached" });
-}
-
-async function assertLocalStorageExist(page: Page, key: string) {
-  await page.waitForFunction((key) => {
-    return localStorage[key] !== undefined;
-  }, key);
-}
-
-async function assertLocalStorageNotExist(page: Page, key: string) {
-  await page.waitForFunction((key) => {
-    return localStorage[key] === undefined;
-  }, key);
-}
-
-async function login(page: Page, username: string, password: string) {
-  const loginResponse = await assertLoginOrRegister({
-    page,
-    expectedPostData: [{ Username: username, Password: password }],
-    trigger: async () => {
-      await getLoginFormButton(page).click();
-    },
-  });
-
-  return loginResponse["Success"];
-}
-
 test("has title", async ({ page }) => {
-  await page.goto(pageURL);
+  const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+  await rssBookmarkrPage.goto();
+
   await expect(page).toHaveTitle("RSS Bookmarkr");
 });
 
 test.describe("single url", () => {
   test("add new url", async ({ page }) => {
-    await page.goto(pageURL);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto();
 
-    const urlInput = getURLInput(page);
-    await urlInput.fill(overreactedURL);
+    await rssBookmarkrPage.addURL(overreactedURL);
 
-    await assertRSSList({
-      page,
-      expectedPostData: [[overreactedURL]],
-      trigger: async () => {
-        await getAddURLButton(page).click();
-      },
-    });
-
-    await expect(urlInput).toHaveValue("");
+    await rssBookmarkrPage.expectHaveURLParam([overreactedURL]);
     await expect(page.getByText(overreactedURL)).toBeVisible();
-    await expect(page).toHaveURL(`${pageURL}/?url=${overreactedURL}`);
   });
 
   test("init url with query param", async ({ page }) => {
-    await assertRSSList({
-      page,
-      expectedPostData: [[overreactedURL]],
-      trigger: async () => {
-        await page.goto(`${pageURL}/?url=${overreactedURL}`);
-      },
-    });
+    const urlParams = [overreactedURL];
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
 
-    const urlInput = getURLInput(page);
+    await rssBookmarkrPage.goto(urlParams);
 
-    await expect(urlInput).toHaveValue("");
+    await rssBookmarkrPage.expectHaveURLParam(urlParams);
     await expect(page.getByText(overreactedURL)).toBeVisible();
-    await expect(page).toHaveURL(`${pageURL}/?url=${overreactedURL}`);
   });
 
   test("delete url", async ({ page }) => {
-    await page.goto(`${pageURL}/?url=${overreactedURL}`);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto([overreactedURL]);
 
-    await getDeleteURLButton(page).click();
-    await expect(page).toHaveURL(pageURL);
+    await rssBookmarkrPage.deleteURL();
+
+    await rssBookmarkrPage.expectNotHaveURLParam();
   });
 });
 
 test.describe("multiple url", () => {
   test("add new url", async ({ page }) => {
-    await page.goto(pageURL);
-
-    const urlInput = getURLInput(page);
-    const addButton = getAddURLButton(page);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto();
 
     for (const rssURL of rssURLS) {
-      await urlInput.fill(rssURL);
-
-      await assertRSSList({
-        page,
-        expectedPostData: [[rssURL]],
-        trigger: async () => {
-          await addButton.click();
-        },
-      });
+      await rssBookmarkrPage.addURL(rssURL);
 
       await expect(page.getByText(rssURL)).toBeVisible();
     }
 
-    await expect(page).toHaveURL(`${pageURL}/?url=${rssURLS.join(",")}`);
+    await rssBookmarkrPage.expectHaveURLParam(rssURLS);
   });
 
   test("init url with query param", async ({ page }) => {
-    await page.goto(`${pageURL}/?url=${rssURLS.join(",")}`);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto(rssURLS);
 
-    await waitLoadingPage(page);
     for (const rssURL of rssURLS) {
       await expect(page.getByText(rssURL)).toBeVisible();
     }
 
-    await expect(page).toHaveURL(`${pageURL}/?url=${rssURLS.join(",")}`);
+    await rssBookmarkrPage.expectHaveURLParam(rssURLS);
   });
 
   test("delete url", async ({ page }) => {
-    await page.goto(`${pageURL}/?url=${rssURLS.join(",")}`);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto(rssURLS);
 
-    await getDeleteURLButton(page).first().click();
+    await rssBookmarkrPage.deleteURL();
+
+    await rssBookmarkrPage.expectHaveURLParam([infoqURL, stackoverflowURL]);
     await expect(page.getByText(infoqURL)).toBeVisible();
     await expect(page.getByText(stackoverflowURL)).toBeVisible();
-    await expect(page).toHaveURL(
-      `${pageURL}/?url=${infoqURL},${stackoverflowURL}`
-    );
   });
 });
 
 test.describe("user authentication", () => {
-  test("login and logout", async ({ page }) => {
-    await page.goto(pageURL);
+  test("login then logout", async ({ page }) => {
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto();
 
-    await getLoginModalButton(page).click();
-    await expect(getLoginFormTitle(page)).toBeVisible();
-
-    await getLoginUsernameField(page).fill(username);
-    await getLoginPasswordField(page).fill(password);
-    await login(page, username, password);
-    await assertLocalStorageExist(page, sessionKey);
-
-    await getLogoutButton(page).click();
-    await assertLocalStorageNotExist(page, sessionKey);
+    await rssBookmarkrPage.login(username, password);
+    await rssBookmarkrPage.logout();
   });
 });
 
 test.describe("authorized user", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(pageURL);
-
-    await getLoginModalButton(page).click();
-
-    await getLoginUsernameField(page).fill(username);
-    await getLoginPasswordField(page).fill(password);
-  });
-
-  test.afterEach(async ({ page }) => {
-    await getLogoutButton(page).click();
-    await assertLocalStorageNotExist(page, sessionKey);
-  });
-
   test("save urls and delete the url", async ({ page }) => {
-    const { UserId: userId } = await login(page, username, password);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto();
+    await rssBookmarkrPage.login(username, password);
 
-    await getURLInput(page).fill(overreactedURL);
-    await getAddURLButton(page).click();
-    await waitLoadingPage(page);
+    await rssBookmarkrPage.addURL(overreactedURL);
+    await rssBookmarkrPage.saveURLs([overreactedURL]);
 
-    const saveUrlsButton = getSaveURLsButton(page);
-
-    await assertSaveRSSUrls({
-      page,
-      expectedPostData: [[userId, [overreactedURL]]],
-      trigger: async () => {
-        await saveUrlsButton.click();
-      },
-    });
-
-    await getDeleteURLButton(page).click();
-
-    await assertSaveRSSUrls({
-      page,
-      expectedPostData: [[userId, []]],
-      trigger: async () => {
-        await saveUrlsButton.click();
-      },
-    });
+    await rssBookmarkrPage.deleteURL();
+    await rssBookmarkrPage.saveURLs([]);
   });
 
   test("subscribe and unsubscribe", async ({ page }) => {
-    const { UserId: userId } = await login(page, username, password);
+    const rssBookmarkrPage = new RSSBokmarkrPage(pageURL, page);
+    await rssBookmarkrPage.goto();
+    await rssBookmarkrPage.login(username, password);
 
-    await getSubscribeModalButton(page).click();
-    await getSubscribeEmailField(page).fill(subscribeEmail);
-
-    await assertSubscribe({
-      page,
-      expectedPostData: [[userId, subscribeEmail]],
-      trigger: async () => {
-        await getSubscribeFormButton(page).click();
-      },
-    });
-
-    await assertUnsubscribe({
-      page,
-      expectedPostData: [subscribeEmail],
-      trigger: async () => {
-        await getUnsubscribeButton(page).click();
-      },
-    });
+    await rssBookmarkrPage.subscribe(subscribeEmail);
+    await rssBookmarkrPage.unsubscribe(subscribeEmail);
   });
 });
