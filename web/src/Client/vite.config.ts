a5dea01d4df91f56edc1b51c4b3abc3806b7a4be
sistracia <156994197+sistracia@users.noreply.github.com>
2025-02-06 16:55:25 +0700

    Bump version

--- web/src/Client/vite.config.ts
+++ web/src/Client/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const proxyPort = process.env.SERVER_PROXY_PORT || "5000";
+const proxyPort = process.env.SERVER_PROXY_PORT || "5050";
 const proxyTarget = "http://localhost:" + proxyPort;
 
 // https://vitejs.dev/config/
